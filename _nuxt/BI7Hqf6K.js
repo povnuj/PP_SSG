0af5ecdbedb4c496c2eb5eb4338699463aaad2ac
M.Losyk <Mykola.Losyk@gmail.com>
2024-05-10 16:02:20 +0300

    v1.4

---
+++ _nuxt/BI7Hqf6K.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./CYHkZsDR.js";const n={name:"Button"},s={class:"btn-vue"};function c(a,r,_,p,u,d){return e(),o("div",s)}const i=t(n,[["render",c]]);export{i as default};
