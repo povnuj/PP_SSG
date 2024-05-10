8db191b0fd1f50d76eb539f62558b56943cb04d9
M.Losyk <Mykola.Losyk@gmail.com>
2024-05-10 21:29:34 +0300

    v1.9

---
+++ _nuxt/B63ynQl0.js
@@ -0,0 +1,1 @@
+import t from"./Dhl_loBh.js";import{_ as n,c as r,b as a,S as c,o as s}from"./Bkko1ijE.js";const _={};function l(e,d){const o=t;return s(),r("div",null,[a(o),c(e.$slots,"default")])}const i=n(_,[["render",l]]);export{i as default};
