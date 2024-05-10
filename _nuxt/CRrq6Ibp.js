3337991a843008f1caf1c241c2e8b13adc17a8f7
M.Losyk <Mykola.Losyk@gmail.com>
2024-05-10 15:18:02 +0300

    v1.3

---
+++ _nuxt/CRrq6Ibp.js
@@ -0,0 +1,1 @@
+import{u as s}from"./Cb11NdIZ.js";import{J as u}from"./Zyj3VY-s.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
