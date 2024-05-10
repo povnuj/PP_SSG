753ad50ebf7a2a01b6cb6116ce3fce96e7d45ef9
M.Losyk <Mykola.Losyk@gmail.com>
2024-05-10 20:35:07 +0300

    v1.7

---
+++ _nuxt/B-jUrrcl.js
@@ -0,0 +1,1 @@
+import{u as s}from"./C2nExkU2.js";import{J as u}from"./C6PJWQ4h.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
---
+++ _nuxt/DgZKiN4s.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./C6PJWQ4h.js";const o={};function t(_,r){return c(),n("h1",null,"Menu Black")}const s=e(o,[["render",t]]);export{s as default};
