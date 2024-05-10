05e9106a9df03ff7dc6839010dba0e1712f1a843
M.Losyk <Mykola.Losyk@gmail.com>
2024-05-10 16:43:11 +0300

    v1.5

---
+++ _nuxt/CAXDMkM8.js
@@ -0,0 +1,1 @@
+import t from"./BqcATCZt.js";import{_ as n,c as a,b as r,S as s,o as c}from"./CtNUaWSH.js";const _={updated(){window.scrollTo(0,0)}};function d(o,p,l,i,f,m){const e=t;return c(),a("div",null,[r(e),s(o.$slots,"default")])}const k=n(_,[["render",d]]);export{k as default};
